4e683a5aa7411962d319a769ad2fb9a8bc840b56
lzh <javakinglzh@users.noreply.github.com>
2018-06-15 16:58:29 +0800

    添加日志信息

--- web_test_consumer/src/main/java/privates/lzh/rpc/web/consumer/controller/WebConsumerController.java
+++ web_test_consumer/src/main/java/privates/lzh/rpc/web/consumer/controller/WebConsumerController.java
@@ -1,5 +1,7 @@
 package privates.lzh.rpc.web.consumer.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,22 +18,21 @@ import privates.lzh.rpc.web.consumer.common.ServerResponse;
 @Controller
 public class WebConsumerController
 {
-    /**
-     * invoke RPC test
-     */
+	private final Logger log = LoggerFactory.getLogger(WebConsumerController.class);
 	@Autowired
 	IUserService userServie;
+	/**
+	 * invoke RPC test
+	 */
     @RequestMapping(value = "/rpctest")
     @ResponseBody     
     public ServerResponse<String> rpcTest()
     {
-    	System.out.println("...web consumer controller...");
-    	
+    	log.debug("...web consumer controller...");
     	UserInfo info = new UserInfo();
     	info.setAge(10);
     	info.setName("lzh");
     	int result = userServie.addUser(info);
-    	
     	String desc = result==0 ? "add Success": "add fail";
     	ServerResponse<String> res = new ServerResponse<String>();
     	res.setDesc(desc);
