f834a6138b8f9982b2eaa9e7a03bb517dd927801
lzh <javakinglzh@users.noreply.github.com>
2018-06-15 16:21:08 +0800

    修改RootBeanDefinition相关方法

--- rpc_core/src/main/java/privates/lzh/rpc/core/consumer/ConsumerBeanFactory.java
+++ rpc_core/src/main/java/privates/lzh/rpc/core/consumer/ConsumerBeanFactory.java
@@ -1,17 +1,32 @@
 package privates.lzh.rpc.core.consumer;
 
+import java.lang.reflect.InvocationHandler;
+
 import org.springframework.beans.factory.FactoryBean;
 
+import privates.lzh.rpc.core.proxy.ConsumerInterfaceProxyUtil;
+import privates.lzh.rpc.core.proxy.ConsumerInvokeHandler;
+
+/****
+ * 生成 RPC 接口代理bean
+ * @author LZH
+ *
+ */
 public class ConsumerBeanFactory implements FactoryBean<Object> 
 {
-
+	private Class<?> clazz;
+	private InvocationHandler handler = new ConsumerInvokeHandler();
+	
 	public Object getObject() throws Exception 
 	{
-		return null;
+		System.out.println("clazz is " + clazz);
+		ClassLoader loader = this.getClass().getClassLoader();
+		return ConsumerInterfaceProxyUtil.newProxyInstance(loader, clazz, handler);
 	}
 
-	public Class<?> getObjectType() {
-		return null;
+	public Class<?> getObjectType() 
+	{
+		return clazz;
 	}
 
 	public boolean isSingleton() 
@@ -19,4 +34,13 @@ public class ConsumerBeanFactory implements FactoryBean<Object>
 		return true;
 	}
 
+	public Class<?> getClazz() 
+	{
+		return clazz;
+	}
+
+	public void setClazz(Class<?> clazz) 
+	{
+		this.clazz = clazz;
+	}
 }
--- rpc_core/src/main/java/privates/lzh/rpc/core/consumer/ConsumerRPCManager.java
+++ rpc_core/src/main/java/privates/lzh/rpc/core/consumer/ConsumerRPCManager.java
@@ -1,5 +1,11 @@
 package privates.lzh.rpc.core.consumer;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.imageio.spi.RegisterableService;
+
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
@@ -8,7 +14,7 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProce
 import org.springframework.beans.factory.support.RootBeanDefinition;
 
 /***
- * 
+ * 动态注册RPC接口的BeanDefinition
  * @author lzh
  *
  */
@@ -16,7 +22,7 @@ public class ConsumerRPCManager implements BeanDefinitionRegistryPostProcessor
 {
 	// RPC interface package to be scanned
 	private String scanPack;
-	
+	private Map<String, String> rpcServices;
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException 
 	{
 		
@@ -24,11 +30,40 @@ public class ConsumerRPCManager implements BeanDefinitionRegistryPostProcessor
 
 	public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException 
 	{
-		System.out.println("....postprocessor....");
-		BeanDefinition definition = new RootBeanDefinition();
-		definition.setLazyInit(false);
-		definition.setScope("singleton");
+		System.out.println("....register start....");
 		
+		Set<Entry<String, String>> entrySet = rpcServices.entrySet();
+		for(Entry<String, String> entry:entrySet)
+		{
+			String name      = entry.getKey();
+			String className = entry.getValue();
+			try 
+			{
+				Class<?> clazz = Class.forName(className);
+				RootBeanDefinition definition = new RootBeanDefinition();
+				definition.setBeanClass(ConsumerBeanFactory.class);
+				definition.setLazyInit(true);
+				definition.setScope(BeanDefinition.SCOPE_SINGLETON);
+				definition.getPropertyValues().addPropertyValue("clazz", clazz);
+				registry.registerBeanDefinition(name, definition);
+				System.out.println("....register class : " + clazz);
+			} catch (ClassNotFoundException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("....register end....");
+	}
+	
+	public Map<String, String> getRpcServices() 
+	{
+		return rpcServices;
+	}
+
+	public void setRpcServices(Map<String, String> rpcServices) 
+	{
+		this.rpcServices = rpcServices;
 	}
 
 	public String getScanPack() 
