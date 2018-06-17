a279c8ab535aa6bbdda0ab1ddcaa513f35aff42e
lzh <javakinglzh@users.noreply.github.com>
2018-06-17 09:32:02 +0800

    修改注解

--- rpc_core/src/main/java/privates/lzh/rpc/core/annotation/RPCService.java
+++ rpc_core/src/main/java/privates/lzh/rpc/core/annotation/RPCService.java
@@ -1,7 +1,18 @@
 package privates.lzh.rpc.core.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/***
+ * identify an interface to provide RPC service
+ * @author LZH
+ *
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RPCService 
 {
-
+	
 }
